dadf420f7524b90ef1fa0ac3a0773fc1743577b3
Kenneth Freitas <kennethmf@outlook.com>
2021-06-12 14:22:52 -0300

    building alert

--- static/js/3.6bcc1cff.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpapp=this.webpackJsonpapp||[]).push([[3],{39:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return h}));var r,c=n(9),s=n(10).b.header(r||(r=Object(c.a)(["\n    width: 100%;\n    height: 300px;\n    border-bottom: 1px solid #313131;\n"]))),i=n(1),b=function(){return Object(i.jsxs)(i.Fragment,{children:[Object(i.jsx)("h1",{children:":)"}),Object(i.jsx)(s,{})]})};function h(){return Object(i.jsx)(i.Fragment,{children:Object(i.jsx)(b,{})})}}}]);
-//# sourceMappingURL=3.6bcc1cff.chunk.js.map
